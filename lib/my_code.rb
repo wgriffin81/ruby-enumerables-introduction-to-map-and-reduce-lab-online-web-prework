d96f65f6dd1ae85b904dbc6c10006826d2b082c1
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 05:32:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,10 +53,17 @@ def reduce_to_total(source_array, starting_point)
     new += source_array[i]
     i += 1 
   end
-  return new
+  new
 end 
 
 def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length 
+    return false if !source_array[i]
+    i += 1 
+  end 
+  return true 
+  
 end 
 
 def reduce_to_any_true(source_array)
