ecc178e5498dd526fb409035e6c211251ce7c0a4
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 05:15:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,14 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-  
+  new_array = []
+  i = 0 
+  while i < source_array.size do 
+    #element = source_array[i]
+    new_array << (-1 * source_array[i])
+    i += 1 
+  end
+  new_array
 end
 
 
