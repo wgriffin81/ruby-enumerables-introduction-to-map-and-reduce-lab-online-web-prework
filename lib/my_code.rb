f97c6491193a0c108aa5d84608b0d113892a4e7a
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 05:33:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -69,7 +69,7 @@ end
 def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.length 
-    return true if !source_array[i]
+    return true if source_array[i]
     i += 1 
   end 
   return false  
