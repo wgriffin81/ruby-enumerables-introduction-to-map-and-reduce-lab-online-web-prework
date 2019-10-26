0cbf088311debf1a495cdc250634343cad384115
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 05:22:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ def reduce_to_total(source_array, starting_point)
   i = 0 
   while i < source_array.size do 
     current_element = source_array[i]
-    new_array << (total + current_element[i])
+    new_array << (starting_point + current_element[i])
     i += 1 
   end
   new_array
