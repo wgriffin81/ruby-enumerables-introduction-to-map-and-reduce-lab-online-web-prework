617488c8bb06b8087abeb9682ba90fb2be2729c5
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 05:12:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,13 @@
 # My Code here....
 
 def map_to_negativize(source_array)
-  
+  new_array = []
+  i = 0 
+  while i < source_array.size do 
+    new_array << (-1 * source_array[i])
+    i += 1 
+  end
+  new_array
 end
   
 def map_to_no_change(source_array)
